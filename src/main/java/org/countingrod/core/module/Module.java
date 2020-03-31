87073e3882e83f452c058fe13e9b5b7d56628ae7
yaesey <18177114151@163.com>
2020-03-31 18:00:40 +0800

    更改包路径

---
+++ src/main/java/org/countingrod/core/module/Module.java
@@ -0,0 +1,27 @@
+package CountingRod.core.module;
+
+import java.lang.annotation.Retention;
+import java.util.Locale;
+
+public @interface Module {
+    /**
+     * Your module name
+     * A example : "LaTeXLoader". allow case but can't has any space
+     *
+     * But remember, CountingRod is use module <b>id</b> to manage
+     * module <b>not module name</b>,if you didn't set your module
+     * id, as you see, we will convert uppercase letters of
+     * name to id as lowercase.
+     *
+     * 你的模块名称
+     * 一个例子是 ： "LaTex Loader" 。允许大小写但不能有空格
+     *
+     * 但是请记住：CountingRod 是使用模块的 <b<>id</> 来管理模块<b>而不是
+     * 模块名</b>， 如果你未设定你的模块 id ，那么我们将会将 Name 强制小写作为id
+     */
+    String moduleName() default "";
+
+    String moduleID() default "";
+
+    String version() default "";
+}
