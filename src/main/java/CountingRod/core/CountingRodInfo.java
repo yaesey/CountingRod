61d2d1f6bc7fe80046d79afaab97f8e48817819a
yaesey <18177114151@163.com>
2020-03-31 17:59:54 +0800

    为多模块项目做准备

--- src/main/java/CountingRod/core/CountingRodInfo.java
+++
@@ -1,7 +0,0 @@
-package CountingRod.core;
-
-public class CountingRodInfo {
-    public static final String NAME = "CountingRodMain";
-    public static final String ID = "counting_rod_main";
-    public static final String VERSION = "0.0.1";
-}
